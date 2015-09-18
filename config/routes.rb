ce3ca960b90ccca617b64f266417bf57bc42c1c3
MatheusGodinho <matheus.godinho@outlook.com>
2015-09-18 02:18:02 -0300

    Refactoring the presentations pages, to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'homepage#index'
+  root 'static_pages#home'
+
+  get 'static_pages/about'
 
 
   # Example of regular route:
