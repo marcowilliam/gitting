263da242f42ea72a42fd79a26e18cf0e1d76e815
MatheusGodinho <matheus.godinho@outlook.com>
2015-09-19 13:33:28 -0300

    Add comentaries to the Static_page_controler test file

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,17 @@
+# Class to test methods from Static_pages_controller
+
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
+# Testing the website navigation and checking the title of pages to Home page
   test "should get home" do
     get :home
     assert_response :success
      assert_select "title", "Pagina Inicial"
   end
 
+# Testing the website navigation and checking the title of pages to About page
   test "should get about" do
     get :about
     assert_response :success
