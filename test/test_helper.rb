3c3672259b2a1d34f1bf03c1d06398547a257ae8
MatheusGodinho <matheus.godinho@outlook.com>
2015-09-19 13:23:50 -0300

    Add class comentaries

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
+# Class from Rails framework to configure test setups and tools
+
+#Configuring tools and gems to be used in tests
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
