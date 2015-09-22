fac7693a41784c351c3de038d19c9d8cd214afad
MatheusGodinho <matheus.godinho@outlook.com>
2015-09-21 21:44:57 -0300

    Add is_admin boolean to user model

--- db/migrate/20150922001956_create_users.rb
+++ db/migrate/20150922001956_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
     	t.string :username
     	t.string :email
+    	t.boolean :is_admin
     	t.timestamps null: false
     end
   end
