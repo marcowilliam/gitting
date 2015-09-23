c5a3f65e3fc3c697ac059230efd5bcaa0453e772
Marco Silva <marcowilliamdf@gmail.com>
2015-09-23 11:16:03 -0300

    Creating two new filds to User #7

--- db/migrate/20150922001956_create_users.rb
+++ db/migrate/20150922001956_create_users.rb
@@ -3,10 +3,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+    	t.string :user_id
     	t.string :username
     	t.string :email
     	t.boolean :is_admin
     	t.timestamps null: false
+    	t.string :provider
     end
   end
 end
