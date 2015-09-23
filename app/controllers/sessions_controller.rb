79a347605f675f17029acf265b0168ce41f6f7b7
MatheusGodinho <matheus.godinho@outlook.com>
2015-09-23 00:19:06 -0300

    Create the controller Session to make user sessions control #7

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+# This is a controller to have a control for User Sessions in application
+
+class SessionsController < ApplicationController   
+	
+	#Method that represents the create session action 
+	def create     
+		auth = request.env["omniauth.auth"]
+		user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+		session[:user_id] = user.id     
+		redirect_to root_url, :notice => "Signed in!"
+	end
+ 	
+ 	#Method that represents the destroy session action 
+	def destroy
+		session[:user_id] = nil
+		redirect_to root_url, :notice => "Signed out!"
+	end
+ 
+end
