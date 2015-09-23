1ae9c99e41ed1afd43978d42af16a9f9854a8895
MatheusGodinho <matheus.godinho@outlook.com>
2015-09-23 10:09:02 -0300

    Add a method to application with verifies a current user in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # Controller to take actions for all system
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+	helper_method :current_user
+ 
+  	private
+ 
+  	def current_user
+   		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
 end
