aa885357442ee145d09d2d323eda34e85eb72b7e
MatheusGodinho <matheus.godinho@outlook.com>
2015-09-22 23:59:34 -0300

    Add method to login with omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 # This is a model class to represent a system user
 
 class User < ActiveRecord::Base
+
+ def self.create_with_omniauth(auth)
+    create! do |user|
+      user.email = auth["info"]["email"]
+      user.username = auth["info"]["name"]
+    end
+  end
+
 end
