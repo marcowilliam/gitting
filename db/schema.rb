e019aa0651b381b5037a905c18dc7a0e294f2c27
Marco Silva <marcowilliamdf@gmail.com>
2015-09-23 11:03:50 -0300

    centralizing images on homepage

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150922010232) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
+    t.boolean  "is_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
