841f24988f5a1bef6e38119f3faca12095f44475
Marco Silva <marcowilliamdf@gmail.com>
2015-09-24 18:57:26 -0300

    adding setup_mail.rb for conneting with SMTP protocol

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,13 +4,24 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new(contact_params)
 
     if @contact.valid?
+      ContactMailer.contact_message(params[:contact]).deliver_now
+      flash[:notice] = 'Mensagem enviado com sucesso'
       redirect_to :action => 'new'
       return  
     end
 
+
     render :action => 'new'
   end
+
+  private
+
+  def contact_params
+    allow = [:name, :subject, :message, :email]
+    params.require(:contact).permit(allow)
+  end
+
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,8 @@
-class ContactMailer < ApplicationMailer
+class ContactMailer < ActionMailer::Base
+  default :from => 'email_remetente@gmail.com'
+
+  def contact_message(contact)
+    @contact = contact
+    mail(:to => 'gittingapplication@gmail.com', :subject => 'Mensagem de Contato')
+  end
 end
