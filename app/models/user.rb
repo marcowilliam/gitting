f06ee0f9d66da10da55d3204433e6caca90d2ac6
MatheusGodinho <matheus.godinho@outlook.com>
2015-09-28 15:19:18 -0300

    Create validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
 	#creating a relation with table authentications
 	has_many :authentications
 
+	#make a validation for elements in model
+	validates_presence_of :email, :username
+	validates_associated :authentications
+	validates_uniqueness_of :email
+
 end
