ae0bdbed96167479ef4bb65543542ee3ff255935
Tâmara Barbosa <tamara-bt@hotmail.com>
2015-10-01 21:51:44 -0300

    Configuring files to tests

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+RSpec.configure do |config|
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_filter '/config/'
+    add_filter '/lib/'
+    add_filter '/vendor/'
+   
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Models', 'app/models'
+    add_group 'Helpers', 'app/helpers'
+    add_group 'Mailers', 'app/mailers'
+    add_group 'Views', 'app/views'
+  end
+  
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+end
