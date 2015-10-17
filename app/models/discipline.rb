1c5e48a9e9486d9568802b591eff911d63c82fcc
MatheusGodinho <matheus.godinho@outlook.com>
2015-10-17 09:33:46 -0300

    After new model changing, this is a change to relationchip of disciplines

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -2,7 +2,6 @@
 # Description :: Class Discipline is a model class
 # to represent discipline, a users group created by admins
 class Discipline < ActiveRecord::Base
-	has_many :students
-	has_many :teachers
-	
+	has_many :inscriptions
+	belongs_to :teachers
 end
