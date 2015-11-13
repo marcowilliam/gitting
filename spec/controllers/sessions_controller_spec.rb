e314fb01291881cac78d6b9efc12d86abd140aba
Tâmara Barbosa <tamara-bt@hotmail.com>
2015-11-13 00:20:40 -0200

    testing contacts controler

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe SessionsController, type: :controller do
 	before do
     request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
   end
- 
+
+ describe "#new" do
+  get :new, {}, {}
+  response.should redirect_to "auth/github"
+
+ end
   describe "#create" do
  
     it "should successfully create a user" do
