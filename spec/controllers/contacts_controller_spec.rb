749edede6b9a7ed61e584241166e2254c7630e23
Tâmara Barbosa <tamara-bt@hotmail.com>
2015-11-13 00:10:31 -0200

    testing contacts controler

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -12,6 +12,14 @@ RSpec.describe ContactsController, type: :controller do
 
 	let(:valid_session) { {} }
 
+	
+describe "GET new" do
+    it "assigns a new contact as @contact" do
+      get :new, {}, valid_session
+     
+    end
+  end
+
 	describe "POST #create" do
 	    context "with valid params" do
 	      it "assigns a newly created contact as @contact" do
