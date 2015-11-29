e42cd310c5c614ba2cf132a07f7aa5af7bd5ac1a
Marco Silva <marcowilliamdf@gmail.com>
2015-11-29 19:24:38 -0200

    commeting helpers that were forgotten

--- app/helpers/rates_helper.rb
+++ app/helpers/rates_helper.rb
@@ -1,2 +1,6 @@
+# File name :: rates_helper.rb
+# Description :: Class RatesHelper from Rails framework
+# to configure rates setups and tools
+
 module RatesHelper
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
+# File name :: user_pages_helper.rb
+# Description :: Class UsersHelper from Rails framework
+# to configure users setups and tools
+
 module UsersHelper
 end
