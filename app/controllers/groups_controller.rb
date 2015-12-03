0310dda822ebac35d6c659458caf95b849921855
Tâmara Barbosa <tamara-bt@hotmail.com>
2015-12-03 19:10:05 -0300

    creating methods and params to groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,28 @@
 # File name :: groups_controller.rb
-# Description :: GroupsController is a controller 
+# Description :: GroupsController is a controller
 # to take actions for all groups.
 
 class GroupsController < ApplicationController
+
+  # Method to create a group instance
+  def new
+  end
+
+  # Method to show the group details
+  def show
+     @user = Group.find(params[:id])
+  end
+
+  # Defining the Class params
+  # @params project_name => name of the group project
+  #         project_description => description of the group project
+  #         source => repository of the group with the source code
+  private
+
+  def group_params
+    params.
+		require(:group).
+		permit(:project_name,:project_description,:source)
+  end
+
 end
